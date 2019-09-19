787bdbbc3748fa95bc6bc6756d61dc5085dbd542
= <=forhadmethun@gmail.com>
2019-09-19 23:08:54 +0300

    README.md file enhanced, fix in statistics

--- src/main/java/com/forhadmethun/fuelconsumptionmanagement/DatabaseSeeder.java
+++ src/main/java/com/forhadmethun/fuelconsumptionmanagement/DatabaseSeeder.java
@@ -20,13 +20,14 @@ public class DatabaseSeeder implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         List<RegistrationEntity> registrationEntities = new ArrayList<>();
-        registrationEntities.add(new RegistrationEntity(1L,"D", 10.10, 12.5, "12345",01,02,2019));
-        registrationEntities.add(new RegistrationEntity(2L,"D", 10.10, 12.5, "12345",01,03,2016));
+        registrationEntities.add(new RegistrationEntity(1L,"D", 10.10, 12.5, "11111",01,02,2019));
+        registrationEntities.add(new RegistrationEntity(2L,"D", 10.10, 12.5, "11111",01,03,2016));
+        registrationEntities.add(new RegistrationEntity(0L,"E", 12.10, 12.5, "11111",01,03,2016));
         registrationEntities.add(new RegistrationEntity(3L,"D", 10.10, 12.5, "12345",01,03,2016));
         registrationEntities.add(new RegistrationEntity(4L,"D", 10.10, 12.5, "12345",01,04,2017));
         registrationEntities.add(new RegistrationEntity(0L,"D", 10.10, 12.5, "12345",06,04,2017));
-        registrationEntities.add(new RegistrationEntity(0L,"D", 10.10, 12.5, "12345",07,1,2018));
-        registrationEntities.add(new RegistrationEntity(0L,"D", 10.10, 12.5, "12345",07,15,2018));
+        registrationEntities.add(new RegistrationEntity(0L,"D", 10.10, 12.5, "12346",07,1,2018));
+        registrationEntities.add(new RegistrationEntity(0L,"D", 10.10, 12.5, "12346",07,15,2018));
         registrationRepository.save(registrationEntities);
 
     }
--- src/main/java/com/forhadmethun/fuelconsumptionmanagement/registration/RegistrationRepository.java
+++ src/main/java/com/forhadmethun/fuelconsumptionmanagement/registration/RegistrationRepository.java
@@ -7,7 +7,10 @@ import java.util.List;
 @Repository
 public interface RegistrationRepository extends JpaRepository<RegistrationEntity,Long> {
     RegistrationEntity findByIdEquals(Long id);
+    List<RegistrationEntity> findByDriverId(String driverId);
     List<RegistrationEntity> findByYearEquals(Integer year);
     List<RegistrationEntity> findByYearEqualsAndMonthEquals(Integer year,Integer months);
+    List<RegistrationEntity> findByYearEqualsAndDriverIdEquals(Integer year,String DriverId);
+    List<RegistrationEntity> findByYearEqualsAndMonthEqualsAndDriverIdEquals(Integer year,Integer months, String driverId);
 
 }
